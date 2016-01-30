81ed0de7eb6de6135b1fcad5f522c23bbffe4ba5
Eduardo Soto <easm93@gmail.com>
2016-01-30 10:53:40 -0500

    Workshop 2
    Added Ethan character, simple goal, and example of how to take control
    of ball

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	public int goals;
+
+	// Use this for initialization
+	void Start () {
+		goals = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Gets called everytime this object collides with another collider
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "SoccerBall") {
+			goals++;
+		}
+	}
+
+}
---
+++ Assets/Scripts/ManouverBall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManouverBall : MonoBehaviour {
+
+	public GameObject soccerBall;
+
+	// Use this for initialization
+	void Start () {
+		//soccerBall = GameObject.Find ("Sphere 13");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (transform.position, soccerBall.transform.position) < 3f) {
+			soccerBall.transform.position = transform.position + Vector3.forward*1.5f + Vector3.up*0.5f;
+		}
+	}
+}
